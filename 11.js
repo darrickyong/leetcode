0afc2850b5bb493adccaa06fe98d51fb38f8bb8d
darrickyong <darrickyong@gmail.com>
2020-11-13 17:17:34 -0800

    update

--- 11.js
+++ 11.js
@@ -1,5 +1,20 @@
-var maxArea = function (height) {
+const maxArea = function (height) {
+  let res = 0,
+      left = 0,
+      right = height.length - 1;
 
+  while (left < right) {
+    let lHeight = height[left];
+    let rHeight = height[right];
+    res = Math.max(res, Math.min(lHeight, rHeight) * (right - left));
+    if (lHeight < rHeight) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return res;
 };
 
 const height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
